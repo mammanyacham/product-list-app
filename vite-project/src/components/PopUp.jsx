4112252d06d981a00470d74e88a187c2525e2461
aarondavidreal-lab <aarondavid.real@gmail.com>
2026-02-28 22:42:26 +0100

    correct image path

--- vite-project/src/components/PopUp.jsx
+++ vite-project/src/components/PopUp.jsx
@@ -1,11 +1,11 @@
-import orderConfirmedIcon from "../assets/images/icon-order-confirmed.svg"
+
 
 
 export default function Modal(props) {
     return (
        <div className="modal-container">
             <div className="modal">
-                <img src={orderConfirmedIcon} />
+                <img src="/images/icon-order-confirmed.svg" />
                 <p className="order-confirmed">Order Confirmed</p>
                 <p className="order-note">We hope you enjoy your food!</p>
                 <div className="modal-confirm-cart">
